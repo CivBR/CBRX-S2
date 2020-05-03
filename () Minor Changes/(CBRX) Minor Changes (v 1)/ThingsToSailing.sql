660efd79d6d068cd56718467b5b82dfa67318f19
Limerickarcher <limaeusarcturus@gmail.com>
2020-05-03 13:39:53 -0400

    Update minor changes

--- () Minor Changes/(CBRX) Minor Changes (v 1)/ThingsToSailing.sql
+++
@@ -1,4 +0,0 @@
-UPDATE Technologies SET AllowsEmbarking = 0;
-UPDATE Technologies SET AllowsEmbarking = 1 WHERE Type = 'TECH_SAILING';
-
-UPDATE Buildings SET PrereqTech = 'TECH_SAILING' WHERE Type = 'BUILDING_HARBOR';
