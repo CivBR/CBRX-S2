cf7bfcd994c16b2b4d219697d6a6d716e4493538
Isaac Volpe <isaac.volpe@gmail.com>
2020-05-30 22:31:05 -0500

    Update CBRX_TSLs.lua
    TSL Ready

--- () Utility/CBRX TSLs (v 1)/CBRX_TSLs.lua
+++ () Utility/CBRX TSLs (v 1)/CBRX_TSLs.lua
@@ -1,153 +1,158 @@
 local cbrxTSLs = {}
 
-cbrxTSLs["CIVILIZATION_BABYLON"] = {X=46, Y=0, S="F", O="F"}
-cbrxTSLs["CIVILIZATION_DJ_ICELAND"] = {X=0, Y=0, S="T", O="T"}
-cbrxTSLs["CIVILIZATION_WALES"] = {X=0, Y=0, S="T", O="F"}
-cbrxTSLs["CIVILIZATION_SWEDEN"] = {X=0, Y=0, S="F", O="F"}
-cbrxTSLs["CIVILIZATION_TCM_BOURBON_SPAIN"] = {X=0, Y=0, S="F", O="F"}
-cbrxTSLs["CIVILIZATION_MC_GAUL"] = {X=0, Y=0, S="F", O="F"}
-cbrxTSLs["CIVILIZATION_GREATEREUROPE_GERMANY"] = {X=0, Y=0, S="F", O="F"}
-cbrxTSLs["CIVILIZATION_JFD_TWO_SICILIES"] = {X=0, Y=0, S="T", O="F"}
-cbrxTSLs["CIVILIZATION_SF_KOSOVO"] = {X=0, Y=0, S="F", O="F"}
-cbrxTSLs["CIVILIZATION_UC_TEUTONS"] = {X=0, Y=0, S="F", O="F"}
-cbrxTSLs["CIVILIZATION_FINNS"] = {X=0, Y=0, S="F", O="F"}
-cbrxTSLs["CIVILIZATION_JFD_USSR_LENIN"] = {X=0, Y=0, S="F", O="F"}
-cbrxTSLs["CIVILIZATION_PERM"] = {X=0, Y=0, S="F", O="F"}
-cbrxTSLs["CIVILIZATION_MC_JERUSALEM"] = {X=0, Y=0, S="F", O="F"}
-cbrxTSLs["CIVILIZATION_US_GEORGIA"] = {X=0, Y=0, S="F", O="F"}
-cbrxTSLs["CIVILIZATION_TCM_PTOLEMIES"] = {X=0, Y=0, S="F", O="F"}
-cbrxTSLs["CIVILIZATION_JFD_VANDALS_GENSERIC"] = {X=0, Y=0, S="T", O="F"}
-cbrxTSLs["CIVILIZATION_MC_TUAREG"] = {X=0, Y=0, S="F", O="F"}
-cbrxTSLs["CIVILIZATION_DMS_BURKINA_FASO"] = {X=0, Y=0, S="F", O="F"}
-cbrxTSLs["CIVILIZATION_CL_NIGERIA"] = {X=0, Y=0, S="F", O="F"}
-cbrxTSLs["CIVILIZATION_DMS_ZAIRE"] = {X=0, Y=0, S="F", O="F"}
-cbrxTSLs["CIVILIZATION_THP_SOMALIA"] = {X=0, Y=0, S="F", O="F"}
-cbrxTSLs["CIVILIZATION_SENSHI_ZANZIBAR"] = {X=0, Y=0, S="T", O="F"}
-cbrxTSLs["CIVILIZATION_THP_NAMIBIA"] = {X=0, Y=0, S="F", O="F"}
-cbrxTSLs["CIVILIZATION_CSLESOTHO"] = {X=0, Y=0, S="F", O="F"}
-cbrxTSLs["CIVILIZATION_KURD"] = {X=0, Y=0, S="F", O="F"}
-cbrxTSLs["CIVILIZATION_UC_HEJAZ"] = {X=0, Y=0, S="F", O="F"}
-cbrxTSLs["CIVILIZATION_JWW_UZBEKISTAN"] = {X=0, Y=0, S="F", O="F"}
-cbrxTSLs["CIVILIZATION_JFD_PUNJAB"] = {X=0, Y=0, S="F", O="F"}
-cbrxTSLs["CIVILIZATION_BHUTAN"] = {X=0, Y=0, S="F", O="F"}
-cbrxTSLs["CIVILIZATION_MC_CHOLA"] = {X=0, Y=0, S="F", O="F"}
-cbrxTSLs["CIVILIZATION_EW_PARG"] = {X=0, Y=0, S="F", O="F"}
-cbrxTSLs["CIVILIZATION_THP_NORTHYUAN"] = {X=0, Y=0, S="F", O="F"}
-cbrxTSLs["CIVILIZATION_SENSHI_CHUKCHI"] = {X=0, Y=0, S="T", O="F"}
-cbrxTSLs["CIVILIZATION_SENSHI_MANCHU"] = {X=0, Y=0, S="F", O="F"}
-cbrxTSLs["CIVILIZATION_SM_KAKJAPAN"] = {X=0, Y=0, S="T", O="F"}
-cbrxTSLs["CIVILIZATION_TAR_YUAN"] = {X=0, Y=0, S="F", O="F"}
-cbrxTSLs["CIVILIZATION_PB_TAIPING"] = {X=0, Y=0, S="F", O="F"}
-cbrxTSLs["CIVILIZATION_C15_NVIET"] = {X=0, Y=0, S="F", O="F"}
-cbrxTSLs["CIVILIZATION_JFD_LAOS"] = {X=0, Y=0, S="F", O="F"}
-cbrxTSLs["CIVILIZATION_ORG_MALACCA"] = {X=0, Y=0, S="T", O="F"}
-cbrxTSLs["CIVILIZATION_VANA_VOC"] = {X=0, Y=0, S="T", O="F"}
-cbrxTSLs["CIVILIZATION_THP_ANANGU"] = {X=0, Y=0, S="F", O="F"}
-cbrxTSLs["CIVILIZATION_CL_KULIN"] = {X=0, Y=0, S="F", O="F"}
-cbrxTSLs["CIVILIZATION_JFD_HAWAII"] = {X=0, Y=0, S="T", O="T"}
-cbrxTSLs["CIVILIZATION_TAHITI_LS_MOD"] = {X=0, Y=0, S="T", O="T"}
-cbrxTSLs["CIVILIZATION_DENEFIRSTNATION"] = {X=0, Y=0, S="F", O="F"}
-cbrxTSLs["CIVILIZATION_MC_CHINOOK"] = {X=0, Y=0, S="F", O="F"}
-cbrxTSLs["CIVILIZATION_SAS_TONGVA"] = {X=0, Y=0, S="F", O="F"}
-cbrxTSLs["CIVILIZATION_MC_MHA"] = {X=0, Y=0, S="F", O="F"}
-cbrxTSLs["CIVILIZATION_MISSISSIPPI_MOD_TOMATEKH"] = {X=0, Y=0, S="F", O="F"}
-cbrxTSLs["CIVILIZATION_JWW_RIO_GRANDE"] = {X=0, Y=0, S="F", O="F"}
-cbrxTSLs["CIVILIZATION_VANA_NEWNETHERLAND"] = {X=0, Y=0, S="F", O="F"}
-cbrxTSLs["CIVILIZATION_EW_NEUTRAL"] = {X=0, Y=0, S="F", O="F"}
-cbrxTSLs["CIVILIZATION_LEUGI_OLMEC"] = {X=0, Y=0, S="F", O="F"}
-cbrxTSLs["CIVILIZATION_PI_JAMAICA"] = {X=0, Y=0, S="T", O="T"}
-cbrxTSLs["CIVILIZATION_PG_GRANCOLOMBIA"] = {X=0, Y=0, S="F", O="F"}
-cbrxTSLs["CIVILIZATION_SENSHI_MARAJO"] = {X=0, Y=0, S="F", O="F"}
-cbrxTSLs["CIVILIZATION_PB_PALMARES"] = {X=0, Y=0, S="F", O="F"}
-cbrxTSLs["CIVILIZATION_SENSHI_PERUBOLIVIA"] = {X=0, Y=0, S="F", O="F"}
-cbrxTSLs["CIVILIZATION_UC_PARAGUAY"] = {X=0, Y=0, S="F", O="F"}
-cbrxTSLs["CIVILIZATION_MAPUCHE"] = {X=0, Y=0, S="F", O="F"}
-
-
+cbrxTSLs["CIVILIZATION_BABYLON"] = {X = 46, Y = 0, S = "F", O = "F"}
+cbrxTSLs["CIVILIZATION_DJ_ICELAND"] = {X = 0, Y = 84, S = "T", O = "T"}
+cbrxTSLs["CIVILIZATION_WALES"] = {X = 6, Y = 67, S = "T", O = "F"}
+cbrxTSLs["CIVILIZATION_SWEDEN"] = {X = 25, Y = 78, S = "F", O = "F"}
+cbrxTSLs["CIVILIZATION_TCM_BOURBON_SPAIN"] = {X = 9, Y = 52, S = "F", O = "F"}
+cbrxTSLs["CIVILIZATION_MC_GAUL"] = {X = 13, Y = 57, S = "F", O = "F"}
+cbrxTSLs["CIVILIZATION_GREATEREUROPE_GERMANY"] = {X = 21, Y = 65, S = "F", O = "F"}
+cbrxTSLs["CIVILIZATION_JFD_TWO_SICILIES"] = {X = 23, Y = 53, S = "T", O = "F"}
+cbrxTSLs["CIVILIZATION_SF_KOSOVO"] = {X = 28, Y = 55, S = "F", O = "F"}
+cbrxTSLs["CIVILIZATION_UC_TEUTONS"] = {X = 28, Y = 68, S = "F", O = "F"}
+cbrxTSLs["CIVILIZATION_FINNS"] = {X = 31, Y = 78, S = "F", O = "F"}
+cbrxTSLs["CIVILIZATION_JFD_USSR_LENIN"] = {X = 38, Y = 71, S = "F", O = "F"}
+cbrxTSLs["CIVILIZATION_PERM"] = {X = 49, Y = 74, S = "F", O = "F"}
+cbrxTSLs["CIVILIZATION_MC_JERUSALEM"] = {X = 37, Y = 48, S = "F", O = "F"}
+cbrxTSLs["CIVILIZATION_US_GEORGIA"] = {X = 43, Y = 60, S = "F", O = "F"}
+cbrxTSLs["CIVILIZATION_TCM_PTOLEMIES"] = {X = 32, Y = 47, S = "F", O = "F"}
+cbrxTSLs["CIVILIZATION_JFD_VANDALS_GENSERIC"] = {X = 19, Y = 48, S = "T", O = "F"}
+cbrxTSLs["CIVILIZATION_MC_TUAREG"] = {X = 16, Y = 40, S = "F", O = "F"}
+cbrxTSLs["CIVILIZATION_DMS_BURKINA_FASO"] = {X = 16, Y = 32, S = "F", O = "F"}
+cbrxTSLs["CIVILIZATION_CL_NIGERIA"] = {X = 20, Y = 29, S = "F", O = "F"}
+cbrxTSLs["CIVILIZATION_DMS_ZAIRE"] = {X = 27, Y = 22, S = "F", O = "F"}
+cbrxTSLs["CIVILIZATION_THP_SOMALIA"] = {X = 43, Y = 27, S = "F", O = "F"}
+cbrxTSLs["CIVILIZATION_SENSHI_ZANZIBAR"] = {X = 41, Y = 23, S = "T", O = "F"}
+cbrxTSLs["CIVILIZATION_THP_NAMIBIA"] = {X = 28, Y = 12, S = "F", O = "F"}
+cbrxTSLs["CIVILIZATION_CSLESOTHO"] = {X = 34, Y = 9, S = "F", O = "F"}
+cbrxTSLs["CIVILIZATION_KURD"] = {X = 43, Y = 53, S = "F", O = "F"}
+cbrxTSLs["CIVILIZATION_UC_HEJAZ"] = {X = 41, Y = 40, S = "F", O = "F"}
+cbrxTSLs["CIVILIZATION_JWW_UZBEKISTAN"] = {X = 58, Y = 62, S = "F", O = "F"}
+cbrxTSLs["CIVILIZATION_JFD_PUNJAB"] = {X = 62, Y = 52, S = "F", O = "F"}
+cbrxTSLs["CIVILIZATION_BHUTAN"] = {X = 70, Y = 51, S = "F", O = "F"}
+cbrxTSLs["CIVILIZATION_MC_CHOLA"] = {X = 66, Y = 33, S = "F", O = "F"}
+cbrxTSLs["CIVILIZATION_EW_PARG"] = {X = 60, Y = 77, S = "F", O = "F"}
+cbrxTSLs["CIVILIZATION_THP_NORTHYUAN"] = {X = 78, Y = 76, S = "F", O = "F"}
+cbrxTSLs["CIVILIZATION_SENSHI_CHUKCHI"] = {X = 102, Y = 86, S = "T", O = "F"}
+cbrxTSLs["CIVILIZATION_SENSHI_MANCHU"] = {X = 90, Y = 73, S = "F", O = "F"}
+cbrxTSLs["CIVILIZATION_SM_KAKJAPAN"] = {X = 103, Y = 68, S = "T", O = "F"}
+cbrxTSLs["CIVILIZATION_TAR_YUAN"] = {X = 86, Y = 70, S = "F", O = "F"}
+cbrxTSLs["CIVILIZATION_PB_TAIPING"] = {X = 88, Y = 59, S = "F", O = "F"}
+cbrxTSLs["CIVILIZATION_C15_NVIET"] = {X = 83, Y = 48, S = "F", O = "F"}
+cbrxTSLs["CIVILIZATION_JFD_LAOS"] = {X = 80, Y = 47, S = "F", O = "F"}
+cbrxTSLs["CIVILIZATION_ORG_MALACCA"] = {X = 82, Y = 32, S = "T", O = "T"}
+cbrxTSLs["CIVILIZATION_VANA_VOC"] = {X = 84, Y = 25, S = "T", O = "T"}
+cbrxTSLs["CIVILIZATION_THP_ANANGU"] = {X = 99, Y = 17, S = "F", O = "F"}
+cbrxTSLs["CIVILIZATION_CL_KULIN"] = {X = 106, Y = 8, S = "F", O = "F"}
+cbrxTSLs["CIVILIZATION_JFD_HAWAII"] = {X = 110, Y = 58, S = "T", O = "T"}
+cbrxTSLs["CIVILIZATION_TAHITI_LS_MOD"] = {X = 133, Y = 28, S = "T", O = "T"}
+cbrxTSLs["CIVILIZATION_DENEFIRSTNATION"] = {X = 127, Y = 85, S = "F", O = "F"}
+cbrxTSLs["CIVILIZATION_MC_CHINOOK"] = {X = 120, Y = 72, S = "F", O = "F"}
+cbrxTSLs["CIVILIZATION_SAS_TONGVA"] = {X = 121, Y = 60, S = "F", O = "F"}
+cbrxTSLs["CIVILIZATION_MC_MHA"] = {X = 130, Y = 76, S = "F", O = "F"}
+cbrxTSLs["CIVILIZATION_MISSISSIPPI_MOD_TOMATEKH"] = {X = 136, Y = 68, S = "F", O = "F"}
+cbrxTSLs["CIVILIZATION_JWW_RIO_GRANDE"] = {X = 131, Y = 58, S = "F", O = "F"}
+cbrxTSLs["CIVILIZATION_VANA_NEWNETHERLAND"] = {X = 150, Y = 69, S = "F", O = "F"}
+cbrxTSLs["CIVILIZATION_EW_NEUTRAL"] = {X = 145, Y = 73, S = "F", O = "F"}
+cbrxTSLs["CIVILIZATION_LEUGI_OLMEC"] = {X = 133, Y = 51, S = "F", O = "F"}
+cbrxTSLs["CIVILIZATION_PI_JAMAICA"] = {X = 146, Y = 48, S = "T", O = "T"}
+cbrxTSLs["CIVILIZATION_PG_GRANCOLOMBIA"] = {X = 145, Y = 40, S = "F", O = "F"}
+cbrxTSLs["CIVILIZATION_SENSHI_MARAJO"] = {X = 157, Y = 38, S = "F", O = "F"}
+cbrxTSLs["CIVILIZATION_PB_PALMARES"] = {X = 165, Y = 32, S = "F", O = "F"}
+cbrxTSLs["CIVILIZATION_SENSHI_PERUBOLIVIA"] = {X = 145, Y = 25, S = "F", O = "F"}
+cbrxTSLs["CIVILIZATION_UC_PARAGUAY"] = {X = 154, Y = 24, S = "F", O = "F"}
+cbrxTSLs["CIVILIZATION_MAPUCHE"] = {X = 145, Y = 14, S = "F", O = "F"}
 
 local observerTechs = {1, 2, 4, 6, 10, 25}
 local sailing = GameInfoTypes["TECH_SAILING"]
 local optics = GameInfoTypes["TECH_OPTICS"]
 
 function addSailing(iPlayer)
-	if not (Game.GetGameTurn() < 3) then return end
-	local pPlayer = Players[iPlayer]
-	if not pPlayer then return end
-	local sCivilizationType = GameInfo.Civilizations[pPlayer:GetCivilizationType()].Type
-	if not sCivilizationType then return end
-	local pTeam = Teams[pPlayer:GetTeam()]
+    if not (Game.GetGameTurn() < 3) then
+        return
+    end
+    local pPlayer = Players[iPlayer]
+    if not pPlayer then
+        return
+    end
+    local sCivilizationType = GameInfo.Civilizations[pPlayer:GetCivilizationType()].Type
+    if not sCivilizationType then
+        return
+    end
+    local pTeam = Teams[pPlayer:GetTeam()]
 
-	if cbrxTSLs[sCivilizationType] then
-		for v in pPlayer:Units() do
-			if v:GetUnitType() == 0 then
-				v:SetXY(cbrxTSLs[sCivilizationType].X, cbrxTSLs[sCivilizationType].Y)
-			end
-		end
-	end
+    if cbrxTSLs[sCivilizationType] then
+        for v in pPlayer:Units() do
+            if v:GetUnitType() == 0 then
+                v:SetXY(cbrxTSLs[sCivilizationType].X, cbrxTSLs[sCivilizationType].Y)
+            end
+        end
+    end
 
-	if cbrxTSLs[sCivilizationType].S == "T" then
-		pTeam:SetHasTech(sailing, true)
-		pTeam:SetHasTech(optics, true)
-	end
+    if cbrxTSLs[sCivilizationType].S == "T" then
+        pTeam:SetHasTech(sailing, true)
+        pTeam:SetHasTech(optics, true)
+    end
 end
 GameEvents.PlayerDoTurn.Add(addSailing)
 
 function setTSLs()
-	if Game.GetGameTurn() < 3 then
-		for iPlayer = 0, GameDefines.MAX_MAJOR_CIVS - 1, 1 do
-			local pPlayer = Players[iPlayer]
-			if not pPlayer then return end
-			local sCivilizationType = GameInfo.Civilizations[pPlayer:GetCivilizationType()].Type
-			local pTeam = Teams[pPlayer:GetTeam()]
-			local currentTeamID = pTeam:GetID()
+    if Game.GetGameTurn() < 3 then
+        for iPlayer = 0, GameDefines.MAX_MAJOR_CIVS - 1, 1 do
+            local pPlayer = Players[iPlayer]
+            if not pPlayer then
+                return
+            end
+            local sCivilizationType = GameInfo.Civilizations[pPlayer:GetCivilizationType()].Type
+            local pTeam = Teams[pPlayer:GetTeam()]
+            local currentTeamID = pTeam:GetID()
 
-			if iPlayer == 0 then
-				for index, val in ipairs(observerTechs) do
-					pTeam:SetHasTech(val, true)
-				end
-			end
+            if iPlayer == 0 then
+                for index, val in ipairs(observerTechs) do
+                    pTeam:SetHasTech(val, true)
+                end
+            end
 
-			for i = 0, Map.GetNumPlots()-1, 1 do
-				local plot = Map.GetPlotByIndex(i);
-				local oldVisibility = plot:GetVisibilityCount(currentTeamID)
+            for i = 0, Map.GetNumPlots() - 1, 1 do
+                local plot = Map.GetPlotByIndex(i)
+                local oldVisibility = plot:GetVisibilityCount(currentTeamID)
 
-				if oldVisibility > 0 then
-					plot:ChangeVisibilityCount(currentTeamID, -1, -1, true, true)
-					plot:SetRevealed(currentTeamID, false)
-					plot:UpdateFog()
-				end
-			end
-			Game.UpdateFOW(true);
-			UI.RequestMinimapBroadcast();
+                if oldVisibility > 0 then
+                    plot:ChangeVisibilityCount(currentTeamID, -1, -1, true, true)
+                    plot:SetRevealed(currentTeamID, false)
+                    plot:UpdateFog()
+                end
+            end
+            Game.UpdateFOW(true)
+            UI.RequestMinimapBroadcast()
 
-			if cbrxTSLs[sCivilizationType] then
-				print(sCivilizationType)
-				for v in pPlayer:Units() do
-					v:SetXY(cbrxTSLs[sCivilizationType].X, cbrxTSLs[sCivilizationType].Y)
-				end
+            if cbrxTSLs[sCivilizationType] then
+                print(sCivilizationType)
+                for v in pPlayer:Units() do
+                    v:SetXY(cbrxTSLs[sCivilizationType].X, cbrxTSLs[sCivilizationType].Y)
+                end
 
-				pPlayer:InitUnit(1, cbrxTSLs[sCivilizationType].X, cbrxTSLs[sCivilizationType].Y)
-				pPlayer:InitUnit(81, cbrxTSLs[sCivilizationType].X, cbrxTSLs[sCivilizationType].Y)
+                pPlayer:InitUnit(1, cbrxTSLs[sCivilizationType].X, cbrxTSLs[sCivilizationType].Y)
+                pPlayer:InitUnit(81, cbrxTSLs[sCivilizationType].X, cbrxTSLs[sCivilizationType].Y)
 
-				if cbrxTSLs[sCivilizationType].O == "T" then
+                if cbrxTSLs[sCivilizationType].O == "T" then
+                    local worker = false
 
-					local worker = false
-
-					for u in pPlayer:Units() do
-						if u:GetUnitType() == 83 then
-							local newTrireme = pPlayer:InitUnit(22, u:GetX(), u:GetY())
-							newTrireme:JumpToNearestValidPlot()
-							u:Kill()
-						elseif u:GetUnitType() == 1 and (cbrxTSLs[sCivilizationType].S == "T" or worker == false) then
-							local newWorkBoat = pPlayer:InitUnit(2, u:GetX(), u:GetY())
-							newWorkBoat:JumpToNearestValidPlot()
-							u:Kill()
-							worker = true
-						end
-					end
-				end
-			end
-		end
-	end
+                    for u in pPlayer:Units() do
+                        if u:GetUnitType() == 83 then
+                            local newTrireme = pPlayer:InitUnit(22, u:GetX(), u:GetY())
+                            newTrireme:JumpToNearestValidPlot()
+                            u:Kill()
+                        elseif u:GetUnitType() == 1 and (cbrxTSLs[sCivilizationType].S == "T" or worker == false) then
+                            local newWorkBoat = pPlayer:InitUnit(2, u:GetX(), u:GetY())
+                            newWorkBoat:JumpToNearestValidPlot()
+                            u:Kill()
+                            worker = true
+                        end
+                    end
+                end
+            end
+        end
+    end
 end
 Events.SequenceGameInitComplete.Add(setTSLs)
