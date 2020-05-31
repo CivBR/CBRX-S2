6a74a7a7aaa0e366b51b636634c779a838e528fe
Limerickarcher <limaeusarcturus@gmail.com>
2020-05-31 18:24:40 -0400

    TSL Update

--- () Utility/CBRX TSLs (v 1)/CBRX_TSLs.lua
+++ () Utility/CBRX TSLs (v 1)/CBRX_TSLs.lua
@@ -3,7 +3,7 @@ local cbrxTSLs = {}
 cbrxTSLs["CIVILIZATION_BABYLON"] = {X = 46, Y = 0, S = "F", O = "F"}
 cbrxTSLs["CIVILIZATION_DJ_ICELAND"] = {X = 0, Y = 84, S = "T", O = "T"}
 cbrxTSLs["CIVILIZATION_WALES"] = {X = 6, Y = 67, S = "T", O = "F"}
-cbrxTSLs["CIVILIZATION_SWEDEN"] = {X = 25, Y = 78, S = "F", O = "F"}
+cbrxTSLs["CIVILIZATION_JFD_SWEDEN_GUSTAV"] = {X = 25, Y = 78, S = "F", O = "F"}
 cbrxTSLs["CIVILIZATION_TCM_BOURBON_SPAIN"] = {X = 9, Y = 52, S = "F", O = "F"}
 cbrxTSLs["CIVILIZATION_MC_GAUL"] = {X = 13, Y = 57, S = "F", O = "F"}
 cbrxTSLs["CIVILIZATION_GREATEREUROPE_GERMANY"] = {X = 21, Y = 65, S = "F", O = "F"}
@@ -40,7 +40,7 @@ cbrxTSLs["CIVILIZATION_TAR_YUAN"] = {X = 86, Y = 70, S = "F", O = "F"}
 cbrxTSLs["CIVILIZATION_PB_TAIPING"] = {X = 88, Y = 59, S = "F", O = "F"}
 cbrxTSLs["CIVILIZATION_C15_NVIET"] = {X = 83, Y = 48, S = "F", O = "F"}
 cbrxTSLs["CIVILIZATION_JFD_LAOS"] = {X = 80, Y = 47, S = "F", O = "F"}
-cbrxTSLs["CIVILIZATION_ORG_MALACCA"] = {X = 82, Y = 32, S = "T", O = "T"}
+cbrxTSLs["CIVILIZATION_ORG_MALACCA"] = {X = 82, Y = 33, S = "T", O = "T"}
 cbrxTSLs["CIVILIZATION_VANA_VOC"] = {X = 84, Y = 25, S = "T", O = "T"}
 cbrxTSLs["CIVILIZATION_THP_ANANGU"] = {X = 99, Y = 17, S = "F", O = "F"}
 cbrxTSLs["CIVILIZATION_CL_KULIN"] = {X = 106, Y = 8, S = "F", O = "F"}
@@ -63,31 +63,27 @@ cbrxTSLs["CIVILIZATION_SENSHI_PERUBOLIVIA"] = {X = 145, Y = 25, S = "F", O = "F"
 cbrxTSLs["CIVILIZATION_UC_PARAGUAY"] = {X = 154, Y = 24, S = "F", O = "F"}
 cbrxTSLs["CIVILIZATION_MAPUCHE"] = {X = 145, Y = 14, S = "F", O = "F"}
 
+--local observerTechs = {GameInfoTypes["TECH_POTTERY"].ID, GameInfoTypes["TECH_ANIMAL_HUSBANDRY"].ID, GameInfoTypes["TECH_MINING"].ID, GameInfoTypes["TECH_CALENDAR"].ID, GameInfoTypes["TECH_MASONRY"].ID, GameInfoTypes["TECH_COMPASS"].ID}
+-- Pottery, Animal Husbandry, Mining, Calendar, Masonry, Compass
 local observerTechs = {1, 2, 4, 6, 10, 25}
 local sailing = GameInfoTypes["TECH_SAILING"]
 local optics = GameInfoTypes["TECH_OPTICS"]
 
 function addSailing(iPlayer)
-    if not (Game.GetGameTurn() < 3) then
-        return
-    end
-    local pPlayer = Players[iPlayer]
-    if not pPlayer then
-        return
-    end
-    local sCivilizationType = GameInfo.Civilizations[pPlayer:GetCivilizationType()].Type
-    if not sCivilizationType then
-        return
-    end
-    local pTeam = Teams[pPlayer:GetTeam()]
-
-    if cbrxTSLs[sCivilizationType] then
-        for v in pPlayer:Units() do
-            if v:GetUnitType() == 0 then
-                v:SetXY(cbrxTSLs[sCivilizationType].X, cbrxTSLs[sCivilizationType].Y)
-            end
-        end
-    end
+    if Game.GetGameTurn() > 2 then return end
+    local player = Players[iPlayer]
+    if not player then return end
+    local sCivilizationType = GameInfo.Civilizations[player:GetCivilizationType()].Type
+    if not sCivilizationType then return end
+	if not cbrxTSLs[sCivilizationType] then return end
+    
+	for v in player:Units() do
+		if v:GetUnitType() == 0 then
+			v:SetXY(cbrxTSLs[sCivilizationType].X, cbrxTSLs[sCivilizationType].Y)
+		end
+	end
+	
+	local pTeam = Teams[player:GetTeam()]
 
     if cbrxTSLs[sCivilizationType].S == "T" then
         pTeam:SetHasTech(sailing, true)
@@ -97,62 +93,59 @@ end
 GameEvents.PlayerDoTurn.Add(addSailing)
 
 function setTSLs()
-    if Game.GetGameTurn() < 3 then
-        for iPlayer = 0, GameDefines.MAX_MAJOR_CIVS - 1, 1 do
-            local pPlayer = Players[iPlayer]
-            if not pPlayer then
-                return
-            end
-            local sCivilizationType = GameInfo.Civilizations[pPlayer:GetCivilizationType()].Type
-            local pTeam = Teams[pPlayer:GetTeam()]
-            local currentTeamID = pTeam:GetID()
+    if Game.GetGameTurn() > 2 then return end
+	for iPlayer = 0, GameDefines.MAX_MAJOR_CIVS - 1, 1 do
+		local player = Players[iPlayer]
+		if not player then return end
+		local sCivilizationType = GameInfo.Civilizations[player:GetCivilizationType()].Type
+		local pTeam = Teams[player:GetTeam()]
+		local currentTeamID = pTeam:GetID()
 
-            if iPlayer == 0 then
-                for index, val in ipairs(observerTechs) do
-                    pTeam:SetHasTech(val, true)
-                end
-            end
+		if iPlayer == 0 then
+			for index, val in ipairs(observerTechs) do
+				pTeam:SetHasTech(val, true)
+			end
+		end
 
-            for i = 0, Map.GetNumPlots() - 1, 1 do
-                local plot = Map.GetPlotByIndex(i)
-                local oldVisibility = plot:GetVisibilityCount(currentTeamID)
+		for i = 0, Map.GetNumPlots() - 1, 1 do
+			local plot = Map.GetPlotByIndex(i)
+			local oldVisibility = plot:GetVisibilityCount(currentTeamID)
 
-                if oldVisibility > 0 then
-                    plot:ChangeVisibilityCount(currentTeamID, -1, -1, true, true)
-                    plot:SetRevealed(currentTeamID, false)
-                    plot:UpdateFog()
-                end
-            end
-            Game.UpdateFOW(true)
-            UI.RequestMinimapBroadcast()
+			if oldVisibility > 0 then
+				plot:ChangeVisibilityCount(currentTeamID, -1, -1, true, true)
+				plot:SetRevealed(currentTeamID, false)
+				plot:UpdateFog()
+			end
+		end
+		Game.UpdateFOW(true)
+		UI.RequestMinimapBroadcast()
 
-            if cbrxTSLs[sCivilizationType] then
-                print(sCivilizationType)
-                for v in pPlayer:Units() do
-                    v:SetXY(cbrxTSLs[sCivilizationType].X, cbrxTSLs[sCivilizationType].Y)
-                end
+		if cbrxTSLs[sCivilizationType] then
+			print(sCivilizationType)
+			for v in player:Units() do
+				v:SetXY(cbrxTSLs[sCivilizationType].X, cbrxTSLs[sCivilizationType].Y)
+			end
 
-                pPlayer:InitUnit(1, cbrxTSLs[sCivilizationType].X, cbrxTSLs[sCivilizationType].Y)
-                pPlayer:InitUnit(81, cbrxTSLs[sCivilizationType].X, cbrxTSLs[sCivilizationType].Y)
+			player:InitUnit(1, cbrxTSLs[sCivilizationType].X, cbrxTSLs[sCivilizationType].Y)
+			player:InitUnit(81, cbrxTSLs[sCivilizationType].X, cbrxTSLs[sCivilizationType].Y)
 
-                if cbrxTSLs[sCivilizationType].O == "T" then
-                    local worker = false
+			if cbrxTSLs[sCivilizationType].O == "T" then
+				local worker = false
 
-                    for u in pPlayer:Units() do
-                        if u:GetUnitType() == 83 then
-                            local newTrireme = pPlayer:InitUnit(22, u:GetX(), u:GetY())
-                            newTrireme:JumpToNearestValidPlot()
-                            u:Kill()
-                        elseif u:GetUnitType() == 1 and (cbrxTSLs[sCivilizationType].S == "T" or worker == false) then
-                            local newWorkBoat = pPlayer:InitUnit(2, u:GetX(), u:GetY())
-                            newWorkBoat:JumpToNearestValidPlot()
-                            u:Kill()
-                            worker = true
-                        end
-                    end
-                end
-            end
-        end
-    end
+				for u in player:Units() do
+					if u:GetUnitType() == 83 then
+						local newTrireme = player:InitUnit(22, u:GetX(), u:GetY())
+						newTrireme:JumpToNearestValidPlot()
+						u:Kill()
+					elseif u:GetUnitType() == 1 and (cbrxTSLs[sCivilizationType].S == "T" or worker == false) then
+						local newWorkBoat = player:InitUnit(2, u:GetX(), u:GetY())
+						newWorkBoat:JumpToNearestValidPlot()
+						u:Kill()
+						worker = true
+					end
+				end
+			end
+		end
+	end
 end
 Events.SequenceGameInitComplete.Add(setTSLs)
